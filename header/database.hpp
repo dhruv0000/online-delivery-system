8a15fb4cc058e137a9d6cf91695c44fd0d60b53c
ManulGoyal <44092699+ManulGoyal@users.noreply.github.com>
2019-11-22 16:50:09 +0530

    created db

---
+++ header/database.hpp
@@ -0,0 +1,11 @@
+#include"utilities.hpp"
+
+namespace Database {
+    vector<Order*> orders;
+    vector<Product*> products;
+    vector<User*> users;
+    User* currentUser;
+    User* admin;
+    double discount;
+    double deliveryCharge;
+};
