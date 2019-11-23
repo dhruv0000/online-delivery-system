2a9292734dcb470df89e59262952a993b81cf358
mananshah27092000 <41318450+mananshah27092000@users.noreply.github.com>
2019-11-24 02:20:59 +0530

    Added New Classes :Password and Address

--- header/utilities.h
+++ header/utilities.h
@@ -105,16 +105,99 @@ class Customer : public User{
   Cart cart;
 };
 
+class Password{
+public:
+    static bool checkStrength(string passwd){
+        int length = passwd.length();
+        int countUpper = 0,countLower = 0,countDigit = 0;
+        for(int i=0;i<len;i++){
+            if(passwd[i] >= 65 && passwd[i] <= 90)
+                countUpper++;
+            if(passwd[i] >= 97 && passwd[i] <= 122)
+                countLower++;
+            if(passwd[i] >= 48 && passwd[i] <= 57)
+                countDigit++;
+        }
+        if(countUpper && countLower && countDigit)
+            return false;
+
+        return true;
+    }
+    static unsigned long hashValue(string passwd){
+        hash<string> h;
+        return h(passwd);
+    }
+
+};
+
+
+class Address{
+private:
+    string building,street,city,state;
+
+public:
+    void storeAddress(){
+        cout<<"Enter all the details in one line each "<<endl;
+        cout<<"Enter your Building/Appartment : ";
+        getline(str,building);
+        cout<<"Enter your Street :\t\t ";
+        getline(str,street);
+        cout<<"Enter your City :\t\t ";
+        getline(str,city);
+        cout<<"Enter your State :\t\t ";
+        getline(str,state);
+    }
+
+    void displayAddress(){
+        cout<<"Building/Appartment : "<<bulding<<endl;
+        cout<<"Street : "<<street<<endl;
+        cout<<"City : "<<city<<endl;
+        cout<<"State : "<<state<<endl;
+    }
+};
+
+
 class User{
   string username;
-  string password;
+  unsigned long password;
   Wallet wallet;
   vector<Order*> orders;
   string account;
-  string address;
-  Type type;
+  Address address;
+  enum Type type;
+public:
+  User(string username, unsigned long password, string account, Address address,Type type){
+    this->username = username;
+    this->password = password;
+    this->account = account;
+    this->address = address;
+    this->type = type;
+    Wallet* newWallet = new Wallet(0);
+    this->wallet = *newWallet;
+
+  }
+  static bool checkUserNameAvailable(string userName){
+    
+    for(auto presentUserNameCheck : Database::users){
+      if(presentUserNameCheck->username == userName)return false;
+    }
+    return true;
+  }
+
+
 };
+
 
 class Wallet{
   double balance;
+public:
+  Wallet(double d){
+    this->balance = balance;
+  }
+  double getBalance(){
+    return this->balance;
+  }
+  void updateBalance(double increment){
+    this->balance = this->balance + increment;
+  }
 };
