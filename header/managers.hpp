9ba78c5099e9bf41925b47ca5641b5545461ac1b
mananshah27092000 <mananshah27092000@gmail.com>
2019-11-28 00:17:21 +0530

    Added vendor addProduct

--- header/managers.hpp
+++ header/managers.hpp
@@ -63,6 +63,7 @@ class ProductManager {
 };
 
 
+
 class UserManager{
 
 
--- login.cpp
+++ login.cpp
@@ -122,6 +122,7 @@ void getUserDetails(string &username,string &password,unsigned long long &value)
 }
 
 void showTopSearch(vector<Product*> v){
+    printFlow();
     for(int i=0;i<v.size();i++){
         cout<<"Product "<<i+1<<endl;
         v[i]->displayProduct();
@@ -131,7 +132,7 @@ void showTopSearch(vector<Product*> v){
 void showFuntionality(int i){
     cout<<i+1<<":Search products"<<endl;
     cout<<i+2<<":Show cart"<<endl;
-    cout<<i+3<<":Add money to wallet"<<endl;
+    cout<<i+3<<":Show wallet balance"<<endl;
     cout<<i+4<<":Logout"<<endl;
 }
 
@@ -144,6 +145,40 @@ void displaySearchProduct(vector<Product*> searchedProduct){
         i++;
     }
 }
+
+void displayVendorChoices(){
+    printFlow();
+    cout<<"1:Add Product to our Shop"<<endl;
+    cout<<"2:Show Pending Orders"<<endl;
+    cout<<"3:Show Wallet Balance"<<endl;
+    cout<<"4:See Reviews and Ratings"<<endl;
+    cout<<"5:Logout"<<endl;
+}
+
+void getProductDetails(string &name,string &type,int &quantity,double &price,string &description){
+    cout<<"\nEnter the followig details"<<endl;
+    cout<<"Name your product name:";
+    getchar();
+    getline(cin,name);
+    cout<<"Enter your product type:";
+    getline(cin,type);
+    cout<<"Enter quantity(must be an integer value):";
+    cin>>quantity;
+    cout<<"Enter price(must be decimal value):";
+    cin>>price;
+    cout<<"Would you liketo add description of your product?Y/N";
+    char check;
+    cin>>check;
+    if(check == 'Y'){
+        cout<<"Enter description in one line :";
+        getline(cin,description);
+    }
+
+}
+
+void displayProductAcceptance()[
+    
+]
 
 int main(){
    
@@ -183,7 +218,10 @@ int main(){
         if(UserManager :: registerUser(username, hashPassword, accountNumber, address, type)){
             cout<<endl;
             cout<<"You have successfully created new account (:"<<endl;
-            cout<<"Please login into account to purchase our products"<<endl;
+            if(type == CUSTOMER)
+                cout<<"Please login into your account to purchase our products"<<endl;
+            else
+                cout<<"Please login into your account to do business with us"<<endl;
             cout<<endl;
             goto SignIn;
         }
@@ -227,15 +265,13 @@ int main(){
         getUserDetails(username,password,hashValue);
 
         if(!UserManager::loginUser(username,hashValue)){
-            cout<<endl;
-            cout<<"You have entered wrong username or password"<<endl;
+            cout<<"\nYou have entered wrong username or password\n\n";
             goto SignIn;
         }
-        cout<<(Database :: currentUser)->getType()<<endl;
+        
         if((Database :: currentUser)->getType() != CUSTOMER){
-            cout<<endl;
-            cout<<"Oops you adisplaySearchProduct(searchProduct);re not CUSTOMER"<<endl;
-            cout<<"Please select proper category"<<endl;
+            cout<<"\nOops you are not CUSTOMER\n";
+            cout<<"Please select proper category\n"<<endl;
             goto SignIn;
         }
 
@@ -266,12 +302,18 @@ int main(){
                 goto CustomerChoices; 
             }
 
-            int productWish;
+            char productWish;
             SearchProductDisplay:
             displaySearchProduct(searchProduct);
-           if(productWish<1 ||productWish>searchProduct.size()){
-                cout<<"\nMatch not found\n\n";
-                goto SearchProductDisplay;
+            cin>>productWish;
+            
+            //For user not to enter random numbers , isko lite lo 
+            //if(productWish<1 ||productWish>searchProduct.size()){
+            //     cout<<"\nMatch not found\n\n";
+            //     goto SearchProductDisplay;
+            // }
+            if(productWish >= '1' && productWish <= (char)(searchProduct.size()+(int)'0')){
+                displayProductAcceptance();
             }
 
 
@@ -286,8 +328,47 @@ int main(){
        
 
     }
-    // else if (wish == 4) {
-    // }
+    else if (wish == '4') {
+        string username,password;
+        unsigned long long hashValue;
+        getUserDetails(username,password,hashValue);
+
+        if(!UserManager :: loginUser(username,hashValue)){
+            cout<<"\nYou have entered wrong username or password\n\n";
+            goto SignIn; 
+        }
+        if((Database :: currentUser)->getType() != VENDOR){
+            cout<<"\nOops you are not VENDOR\n";
+            cout<<"Please select proper category\n"<<endl;
+            goto SignIn;
+        }
+
+        VendorChoices:
+        
+        displayUsername();
+        char vendorWish;
+        displayVendorChoices();
+        cin>>vendorWish;
+        
+        if(vendorWish == '1'){
+            string name,type,description;
+            int quantity;
+            double price;
+
+            getProductDetails(name,type,quantity,price,description);
+
+            if(!(ProductManager :: addProduct(name,type,quantity,price,type))){
+                cout<<"Sorry something went wrong"<<endl;
+                cout<<"Try after Sometime to add product"<<endl;
+                goto VendorChoices;
+            }
+            cout<<"Your product have been added succesfully (:"<<endl;
+            goto VendorChoices;
+
+        }else if(vendorWish == '5')
+            goto SignIn;
+
+    }
     else if (wish != '5'){
         cout<<"You have entered wrong choice"<<endl;
         goto SignIn;
